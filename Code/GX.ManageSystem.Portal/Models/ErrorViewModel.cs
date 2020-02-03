e02519af77315dc34ffaf8c2fe328c7e2cc902d7
moetech-github <github@moetech.cn>
2020-02-03 19:15:05 +0800

    调整目录结构

---
+++ Code/GX.ManageSystem.Portal/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GX.ManageSystem.Portal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
