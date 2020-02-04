665c6320de808316e7914b6963f04aa8e5dbc6f5
moetech-github <github@moetech.cn>
2020-02-04 10:32:03 +0800

    修改标识基架的代码，以从mssql修改为Mysql。

--- Code/GX.ManageSystem.Portal/Startup.cs
+++ Code/GX.ManageSystem.Portal/Startup.cs
@@ -28,8 +28,8 @@ namespace GX.ManageSystem.Portal
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                options.UseMySql(
+                    Configuration.GetConnectionString("AuthConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
